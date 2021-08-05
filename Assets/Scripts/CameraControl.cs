e4b596ced5a89e7cf659ff5d01b996035c0491b3
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-08-05 15:22:08 +0300

    Fix rotation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -16,7 +16,8 @@ public class CameraControl : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+    
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,31 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Variables
-    public  float player_speed = 0f;
-    public Rigidbody player;
-    public Transform cameraTransform;
+	// Variables
+	public float player_speed = 0f;
+	public Rigidbody player;
+	public Transform cameraTransform;
+	public float rotationSpeed = 0f;
 
-    // Start is called before the first frame updat
-    void Start()
-    {
-        player = GetComponent<Rigidbody>();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		player = GetComponent<Rigidbody>();
+		player.rotation = cameraTransform.rotation * Quaternion.AngleAxis(90f, new Vector3(0, 1, 0));
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Get input
-        float x_Axis = Input.GetAxis("Horizontal");
-        float z_Axis = Input.GetAxis("Vertical");
-        Vector3 moveDirection = cameraTransform.forward * z_Axis + cameraTransform.right * x_Axis;
+	// Update is called once per frame
+	void Update()
+	{
+		// Get input
+		float x_Axis = Input.GetAxis("Horizontal");  // A and D
+		float z_Axis = Input.GetAxis("Vertical");  // W and S
+		Vector3 moveDirection = cameraTransform.forward * z_Axis + cameraTransform.right * x_Axis;
 
-        // Move player
-        var get_actual_velocity = player.velocity.y;
-        player.velocity = moveDirection * player_speed; // Update horizontal movement
-        player.velocity = new Vector3(player.velocity.x, get_actual_velocity, player.velocity.z); // Update vertical movement
+		// Move player
+		var get_actual_velocity = player.velocity.y;
+		player.velocity = moveDirection.normalized * player_speed; // Update horizontal movement
+		player.velocity = new Vector3(player.velocity.x, get_actual_velocity, player.velocity.z); // Update vertical movement
 
-        // Fix rotation
-        player.rotation = cameraTransform.rotation * Quaternion.Euler(0,cameraTransform.rotation.y + 90f,0);
-    }
+		// Fix rotation
+		if (moveDirection.magnitude > 10e-3f)
+			player.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-cameraTransform.forward * x_Axis + cameraTransform.right * z_Axis, Vector3.up), Time.deltaTime * rotationSpeed);
+	}
 }
