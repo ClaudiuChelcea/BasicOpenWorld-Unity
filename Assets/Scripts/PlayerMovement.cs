3566771b55a8fbb3d3d98a9ff2349a1471483eaf
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-08-06 10:06:14 +0300

    Fix infinite jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,16 @@ public class PlayerMovement : MonoBehaviour
 	public Rigidbody player;
 	public Transform cameraTransform;
 	public float rotationSpeed = 0f;
+	public float jumpForce = 0f;
+	public CapsuleCollider myCapsule;
+	public float myTreshhold = 0f;
+	private bool is_on_the_ground = true;
 
 	// Start is called before the first frame update
 	void Start()
 	{
 		player = GetComponent<Rigidbody>();
+		myCapsule = GetComponent<CapsuleCollider>();
 		player.rotation = cameraTransform.rotation * Quaternion.AngleAxis(90f, new Vector3(0, 1, 0));
 	}
 
@@ -33,5 +38,25 @@ public class PlayerMovement : MonoBehaviour
 		// Fix rotation
 		if (moveDirection.magnitude > 10e-3f)
 			player.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(-cameraTransform.forward * x_Axis + cameraTransform.right * z_Axis, Vector3.up), Time.deltaTime * rotationSpeed);
+
+		// Jump
+		// See if the player touches the ground ( activate gizmos to see the lines )
+		if (is_on_the_ground == true)
+			Debug.DrawRay(player.transform.position - Vector3.up * myCapsule.height / 2, Vector3.down, Color.green);
+		else
+			Debug.DrawRay(player.transform.position - Vector3.up * myCapsule.height / 2, Vector3.down, Color.red);
+
+		// If he touches the ground
+		if (Physics.Raycast(new Ray(player.transform.position, Vector3.down), myCapsule.height / 2 + myTreshhold))
+		{
+			is_on_the_ground = true;
+			if (Input.GetButtonUp("Jump"))
+			{
+				player.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+			}
+		}
+		else
+			is_on_the_ground = false;
+			
 	}
 }
