c2e6628cc45cd5c9ff71f758ebeec7a9f96cf799
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-08-06 19:52:23 +0300

    Add jump animation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,38 +4,48 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-        // Variables
-        float yaw = 0f;
-        float pitch = 0f;
-        public Transform playerTransform;
-        public float heightToPlayer = 2f;
-        public Vector3 cameraOffset;
-        public float rotationSpeed = 0f;
+	// Variables
+	private float yaw = 0f;
+	private float pitch = 0f;
+	public Transform playerTransform;
+	public float heightToPlayer = 2f;
+	public Vector3 cameraOffset;
+	public float rotationSpeed = 0f;
 
-        // Start is called before the first frame update
-        void Start()
-        {
-                transform.rotation = playerTransform.rotation;
-        }
+	// Start is called before the first frame update
+	void Start()
+	{
+		transform.rotation = playerTransform.rotation;
+	}
 
-        // Update is called once per frame
-        void Update()
-        {
-                yaw += Input.GetAxis("Mouse X");
-                pitch -= Input.GetAxis("Mouse Y");
+	// Update is called once per frame
+	void Update()
+	{
+		yaw += Input.GetAxis("Mouse X");
+		pitch -= Input.GetAxis("Mouse Y");
 
-                // Rotation
-                if(pitch > -20 && pitch <=10)
-                        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(pitch, yaw, 0f), rotationSpeed);
-		else
+		// Rotation
+		SetCameraRotation();
+
+		// Adjust position to follow the player in third person view
+		SetCameraPosition();
+	}
+
+	private void SetCameraPosition()
+	{
+		transform.position = playerTransform.position + transform.TransformDirection(cameraOffset);
+	}
+
+	private void SetCameraRotation()
+	{
+		if (pitch > -20 && pitch <= 10) // Lock camera to certain view on the vertical axis
 		{
-                        if(pitch <= -20)
-                                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-20f, yaw, 0f), rotationSpeed);
-                        else
-                                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(10f, yaw, 0f), rotationSpeed);
-                }
-
-                // Position
-                transform.position = playerTransform.position + transform.TransformDirection(cameraOffset);
-         }
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(pitch, yaw, 0f), rotationSpeed);
+		}
+		else
+		{  // Lock camera`s up and down view
+			if (pitch <= -20) transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(-20f, yaw, 0f), rotationSpeed);
+			else transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(10f, yaw, 0f), rotationSpeed);
+		}
+	}
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,8 +13,8 @@ public class PlayerMovement : MonoBehaviour
 	public CapsuleCollider myCapsule;
 	public float myTreshhold = 0f;
 	private bool is_on_the_ground = true;
-	Animator animator;
-	Vector3 moveDirection;
+	private Animator animator;
+	private Vector3 moveDirection;
 
 	// Start is called before the first frame update
 	void Start()
@@ -28,25 +28,46 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// Get input
-		float x_Axis = Input.GetAxis("Horizontal");  // A and D
-		float z_Axis = Input.GetAxis("Vertical");  // W and S
-		moveDirection = (cameraTransform.forward * z_Axis + cameraTransform.right * x_Axis).normalized * player_speed;
-
-		// Move player
-		Vector3 charSpace = transform.InverseTransformDirection(moveDirection);
-		animator.SetFloat("Forward", charSpace.x);
-		animator.SetFloat("Right", charSpace.z);
+		// Move the player by animation
+		MovePlayer();
 
 		// Player`s rotation
 		myCapsule.transform.rotation = Quaternion.Slerp(myCapsule.transform.rotation, cameraTransform.rotation, 5f);
 
-		// Jump
-		// See if the player touches the ground ( activate gizmos to see the lines )
-		Jump();
+		// Sprint if left shift is being held
+		Sprint();
 
+		// Jump & see if the player touches the ground ( activate gizmos to see the lines )
+		Jump();
 	}
 
+	private void Sprint()
+	{
+		// Increase speed while holding left shift
+		if (Input.GetButton("Fire3"))
+		{
+			player_speed = 10;
+		}
+		else
+		{
+			player_speed = 6;
+		}
+	}
+
+	private void MovePlayer()
+	{
+		// Get input
+		float x_Axis = Input.GetAxis("Horizontal"); // A and D
+		float z_Axis = Input.GetAxis("Vertical"); // W and S
+		moveDirection = (cameraTransform.forward * z_Axis + cameraTransform.right * x_Axis).normalized * player_speed;
+
+		// Move player
+		Vector3 charSpace = transform.InverseTransformDirection(moveDirection);
+		animator.SetFloat("Forward", charSpace.x, 0.075f, Time.deltaTime);
+		animator.SetFloat("Right", charSpace.z, 0.075f, Time.deltaTime);
+	}
+
+	// Change velocity with animator
 	private void OnAnimatorMove()
 	{
 		float get_y = player.velocity.y;
@@ -54,15 +75,15 @@ public class PlayerMovement : MonoBehaviour
 		player.velocity = new Vector3(moveDirection.x, get_y, moveDirection.z);
 	}
 
+	// Jump & check if we are touching the ground
 	private void Jump()
 	{
-		if (is_on_the_ground == true)
-			Debug.DrawRay(player.transform.position, Vector3.down / 5f, Color.green, 2);
-		else
-			Debug.DrawRay(player.transform.position,  Vector3.down / 5f, Color.red, 2);
+		// Draw line to the ground to see if we are on the ground or not
+		if (is_on_the_ground == true) Debug.DrawRay(player.transform.position, Vector3.down / 5f, Color.green, 2);
+		else Debug.DrawRay(player.transform.position, Vector3.down / 5f, Color.red, 2);
 
-		// If he touches the ground
-		if (Physics.Raycast(new Ray(player.transform.position, Vector3.down),  myTreshhold))
+		// If he touches the ground, you can jump
+		if (Physics.Raycast(new Ray(player.transform.position, Vector3.down), myTreshhold)) // send ray
 		{
 			is_on_the_ground = true;
 			if (Input.GetButtonUp("Jump"))
@@ -71,6 +92,18 @@ public class PlayerMovement : MonoBehaviour
 			}
 		}
 		else
+		{ // Otherwise, you can`t jump
 			is_on_the_ground = false;
+		}
+
+		// For the jump animation
+		if (is_on_the_ground)
+		{
+			animator.SetBool("Grounded", true);
+		}
+		else
+		{
+			animator.SetBool("Grounded", false);
+		}
 	}
 }
